a43d13ec8f4d62f481618a0fd6f60fea4e8d4ce9
VOID575 <155201815+VOID575@users.noreply.github.com>
2024-05-05 15:23:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,5 +14,5 @@ def user(name):
     return f"Hello {name}"
 
 if __name__ == "__main__" :
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=8000)
 
